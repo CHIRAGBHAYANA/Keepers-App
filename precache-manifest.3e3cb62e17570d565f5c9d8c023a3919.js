f04601f302a168ad438107095a6a2f9ab68edf74
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-07-09 22:47:34 +0530

    Updates

---
+++ precache-manifest.3e3cb62e17570d565f5c9d8c023a3919.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeebd698995badaf11d944a4133c9d0f",
+    "url": "/Keepers-app/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/Keepers-app/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "c3fabedf6a396b746628",
+    "url": "/Keepers-app/static/js/main.016a8b08.chunk.js"
+  },
+  {
+    "revision": "649f59d06f08f99f7379",
+    "url": "/Keepers-app/static/js/runtime-main.ac9afc4b.js"
+  }
+]);
